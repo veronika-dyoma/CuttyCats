3d8894506e5226e9d793da80f58ac40d06be211d
Вероника Дема < >
2016-11-04 18:34:16 +0300

    start commit

---
+++ src/me/dyoma/Main.java
@@ -0,0 +1,14 @@
+package me.dyoma;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String hello = String.valueOf("Hello ");
+        String cat = String.valueOf("Cat!");
+        StringBuffer result = new StringBuffer(10);
+        result.append(hello).append(cat);
+        System.out.println(result);
+
+    }
+}
