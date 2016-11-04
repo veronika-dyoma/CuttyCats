c1c92ce865cae6e6e178609dfd43a5985a642971
NIKA-PC\Nika <niksya.dioma@yandex.ru>
2016-11-04 19:01:54 +0300

    add cutty feature

--- src/me/dyoma/Main.java
+++ src/me/dyoma/Main.java
@@ -1,14 +1,31 @@
 package me.dyoma;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         String hello = String.valueOf("Hello ");
         String cat = String.valueOf("Cat!");
         StringBuffer result = new StringBuffer(10);
-        result.append(hello).append(cat);
-        System.out.println(result);
 
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String input = bufferedReader.readLine();
+
+
+        if (input != null) {
+            result.append(hello).append(input).append(" ").append(cat);
+        } else {
+            result.append(hello).append(cat);
+        }
+
+        print(result.toString());
+    }
+
+    private static void print(String s) {
+        System.out.println(s);
     }
 }
